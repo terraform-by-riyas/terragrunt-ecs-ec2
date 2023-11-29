2eb54741056efca8015248f532dacd4e94d3c827
Riyas Rawther <62056904+riyas-rawther@users.noreply.github.com>
2023-11-29 09:06:12 +0530

    Update variables.tf

--- infrastructure/modules/ecs-ec2-cluster/variables.tf
+++ infrastructure/modules/ecs-ec2-cluster/variables.tf
@@ -1,8 +1,8 @@
-variable name {}
-variable container_name {}
-variable container_port {}
-variable container_port {}
-variable vpc_id {}
-variable public_subnets {}
-variable vpc_cidr_block {}
-variable vpc_zone_identifier {}
+variable "name" {}
+variable "container_name" {}
+variable "container_port" {}
+variable "container_port" {}
+variable "vpc_id" {}
+variable "public_subnets" {}
+variable "vpc_cidr_block" {}
+variable "vpc_zone_identifier" {}
